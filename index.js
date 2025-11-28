5990df229763a79ca4ade50557ff5d8c7d2b3be0
Tony Nguyen <tonngu137@gmail.com>
2025-11-28 22:10:01 +0100

    add toggle Display PE

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("test");
+import { Collapse } from "bootstrap";
+import { Accordion } from "./src/js/components";
+import { ToggleDisplay } from "./src/js/behaviors/ToggleDisplay-PE";
+
+new Accordion();
+new ToggleDisplay();

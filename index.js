c24b14b1838ccd62286c05b59fab0a9919df723c
Tony Nguyen <tonngu137@gmail.com>
2025-12-17 08:15:29 +0100

    add collapse

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { Collapse } from "bootstrap";
-import { Accordion } from "./src/js/components";
-import { ToggleDisplay } from "./src/js/behaviors/ToggleDisplay-PE";
+import { Collapse } from 'bootstrap';
+// import { Accordion } from './src/js/components';
+import { Collapse as MyCollapse } from './src/js/behaviors/Collapse';
+// import { ToggleDisplay } from './src/js/behaviors/ToggleDisplay';
+// new Accordion();
+// new MyCollapse();
 
-new Accordion();
-new ToggleDisplay();
+// new ToggleDisplay();
+new MyCollapse();
